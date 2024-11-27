bb071e3cc309d4ad95c7ab7cc34289d80f2b75be
Quỳnh Anh <quynhanhhoang572004@gmail.com>
2024-11-27 11:17:28 +0700

    initial

---
+++ passwordGenerator/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [passwordLength, setPasswordlength] = useState(15)
+  const [upperCase, setUppercase] = useState(true)
+  const [lowerCase, setLowercase] = useState(true)
+  const [numbers, setNumbers] = useState(true)
+  
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
